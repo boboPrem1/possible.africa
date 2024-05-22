587571a307e1e666de300c88eded633bf8be2f2e
Bobo Premier <boboprem1@gmail.com>
2024-05-22 02:04:53 +0000

    config cors enabled

--- backend/index.js
+++ backend/index.js
@@ -67,39 +67,39 @@ const aiGeneratorRoutes = require("./endpoints/ai-generator/aiRoutes.js");
 //   },
 // };
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "http://possible.africa",
-      "https://possible.africa",
-      "http://www.possible.africa",
-      "https://www.possible.africa",
-      "http://www.africaleads.ai",
-      "https://www.africaleads.ai",
-      "http://pages.africaleads.ai",
-      "https://pages.africaleads.ai",
-      "http://app.possible.africa",
-      "https://app.possible.africa",
-    ];
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     const allowedOrigins = [
+//       "http://possible.africa",
+//       "https://possible.africa",
+//       "http://www.possible.africa",
+//       "https://www.possible.africa",
+//       "http://www.africaleads.ai",
+//       "https://www.africaleads.ai",
+//       "http://pages.africaleads.ai",
+//       "https://pages.africaleads.ai",
+//       "http://app.possible.africa",
+//       "https://app.possible.africa",
+//     ];
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// };
 // Middleware
 //
-app.use(cors(corsOptions));
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://example.com'); // Remplacez 'http://example.com' par le domaine autorisé
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
+// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Remplacez 'http://example.com' par le domaine autorisé
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 const { protect } = require("./endpoints/auth/authController.js");
 const {
   getAllPostFromAirtable,
