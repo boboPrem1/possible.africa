4b6faa426051741cf47d48af772d79ed12ef4c88
boboPrem1 <boboprem1@gmail.com>
2024-09-18 17:56:41 +0000

    Sending audio filename on the server after recording started

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+import { realTimeTextToSpeech } from "./endpoints/realTime/speechToTextController.js";
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -7,7 +9,7 @@ const multer = require("multer");
 const cron = require("node-cron");
 const fs = require("fs");
 const socketIo = require("socket.io");
-const path = require("path");
+
 // const cron = require("node-cron");
 // import dotenv
 require("dotenv").config();
@@ -28,7 +30,7 @@ require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
 
-const io = socketIo(server, {
+export const io = socketIo(server, {
   cors: {
     origin: "*", // Permet toutes les origines
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Méthodes autorisées
@@ -37,76 +39,9 @@ const io = socketIo(server, {
   },
 });
 
-// Chemin où seront stockés les fichiers audio
-const AUDIO_STORAGE_PATH = path.join(__dirname, "public", "storage", "audios");
+realTimeTextToSpeech();
 
-io.on("connection", (socket) => {
-  const clientId = socket.id;
-  console.log(`Client connecté avec ID: ${clientId}`);
-  let audioStream = null;
 
-  socket.on("recordStarted", (filename) => {
-    console.log(`Enregistrement démarré pour le fichier: ${filename}`);
-
-    audioStream = fs.createWriteStream(
-      path.join(AUDIO_STORAGE_PATH, filename),
-      {
-        flags: "a", // Ajouter les chunks au fichier
-      }
-    );
-    // Vérifier si le stream est bien créé
-    audioStream.on("open", () => {
-      console.log("Flux audio créé et prêt pour l'écriture.");
-      socket.emit("audioStreamCreated", "Flux audio créé");
-    });
-
-    audioStream.on("error", (error) => {
-      console.error("Erreur lors de la création du flux:", error);
-      socket.emit("error", "Erreur lors de la création du flux audio");
-    });
-  });
-
-  socket.on("audioChunk", (chunk) => {
-    try {
-      console.log("Chunk d'audio reçu, en cours d'écriture...");
-
-      // Écrire les chunks dans le fichier
-      audioStream.write(chunk, (err) => {
-        if (err) {
-          console.error("Erreur lors de l'écriture du chunk:", err);
-          socket.emit("error", "Erreur lors de l'écriture du chunk");
-        } else {
-          socket.emit("chunkWritten", "Chunk écrit avec succès");
-        }
-      });
-    } catch (err) {
-      console.error("Erreur lors de la réception du chunk:", err);
-      socket.emit("error", "Erreur lors de la réception du chunk");
-    }
-  });
-
-  socket.on("recordEnd", () => {
-    console.log("Enregistrement terminé.");
-    if (audioStream) {
-      audioStream.end(() => {
-        console.log("Flux audio fermé proprement.");
-        socket.emit(
-          "recordingCompleted",
-          "Enregistrement terminé et fichier fermé"
-        );
-      });
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`Client déconnecté: ${socket.id}`);
-    if (audioStream) {
-      audioStream.end(() => {
-        console.log("Flux audio fermé suite à la déconnexion.");
-      });
-    }
-  });
-});
 // const { Server } = require("socket.io");
 
 const PORT = process.env.SERVER_PORT || 4534;
